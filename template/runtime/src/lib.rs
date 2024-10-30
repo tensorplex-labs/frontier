864dffbdeff4402fb76c4acb31a26b8f123fe8e4
Keith <keith@opentensor.dev>
2024-10-30 15:25:06 +0900

    Fix permissioned smart contract creation

--- template/runtime/src/lib.rs
+++ template/runtime/src/lib.rs
@@ -924,6 +924,7 @@ impl_runtime_apis! {
 					_ => (None, None),
 				};
 
+			let whitelist = pallet_evm::WhitelistedCreators::<Runtime>::get();
 			<Runtime as pallet_evm::Config>::Runner::create(
 				from,
 				data,
@@ -933,6 +934,7 @@ impl_runtime_apis! {
 				max_priority_fee_per_gas,
 				nonce,
 				access_list.unwrap_or_default(),
+				whitelist,
 				false,
 				true,
 				weight_limit,
